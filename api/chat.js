0e92e254172336b737afaabe81a8569878fad931
Matth1 <106870686+Matt0801@users.noreply.github.com>
2025-06-03 13:44:51 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método não permitido' });
-        console.log(process.env.key)
     }
     
     try {
