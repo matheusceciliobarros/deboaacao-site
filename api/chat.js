6900d2888112998864ea6d7f94a386e25fca5b46
Matth1 <106870686+Matt0801@users.noreply.github.com>
2025-06-03 13:43:40 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,10 +1,10 @@
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método não permitido' });
+        console.log(process.env.key)
     }
     
     try {
-        console.log(process.env.key)
         const resposta = await fetch('https://projeto-deboacao.onrender.com', {
             method: 'POST',
             headers: {
